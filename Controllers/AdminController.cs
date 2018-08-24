dd5642f236ec26b9ba7b6641f4b5a6e55053bdf2
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2018-08-23 20:47:25 -0700

    Scaffold for Admin Pages

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseApp.Controllers
+{
+    [Route("")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            // TODO: Admin Home Page
+            return View();
+        }
+
+        [Route("accounts/list/")]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        [Route("accounts/create/")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("accounts/edit/{name}")]
+        public IActionResult Edit(string name)
+        {
+            return View();
+        }
+
+        [Route("accounts/delete/{name}")]
+        public IActionResult Delete(string name)
+        {
+            return View();
+        }
+    }
+}
