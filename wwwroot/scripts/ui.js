205d70dbd1ff9fc937187fb659e7de5e188a1e3a
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2018-08-28 10:37:50 -0700

    Filter works in /form/index/

--- wwwroot/scripts/ui.js
+++ wwwroot/scripts/ui.js
@@ -1,3 +1,6 @@
+document.querySelector("#search-input")
+    .addEventListener("keyup", search, false);
+
 function search()
 {
     const INPUT = document.querySelector("#search-input");
@@ -7,17 +10,23 @@ function search()
     const TABLE = document.querySelector("#expense-table");
     const TR = TABLE.querySelectorAll("tbody tr:not(#empty-search-result)");
 
+    var filterType = getSearchType();
+
     var notDisplayed = 0;
 
     // Loop through all table rows, and hide those that don't match
     // the search query
     for (let i = 0; i < TR.length; i++)
     {
+        var tdStatus = TR[i].querySelectorAll("td")[2].innerHTML.toLowerCase();
         const TD = TR[i].querySelectorAll("td")[searchingByTitle ? 1 : 0];
         if (TD)
         {
-            if (TD.innerHTML.toLowerCase().indexOf(filter) > -1)
+            if (TD.innerHTML.toLowerCase().indexOf(filter) > -1 &&
+                (filterType == "none" || tdStatus == filterType))
+            {
                 TR[i].style.display = "";
+            }
             else
             {
                 TR[i].style.display = "none";
@@ -49,4 +58,28 @@ function search()
     }
     else if (notDisplayed != TR.length && null != TBODY.querySelector("#empty-search-result"))
         TBODY.removeChild(TBODY.querySelector("#empty-search-result"));
+}
+
+function getSearchType()
+{
+    const RADIO_GROUPS = document.querySelectorAll(".filter .radio-group");
+    console.log(RADIO_GROUPS);
+    for (let i = 0; i < RADIO_GROUPS.length; i++)
+    {
+        const INPUT = RADIO_GROUPS[i].querySelector("input");
+        console.log("Checking " + INPUT.getAttribute("value") + "...");
+        if (INPUT.checked)
+        {
+            console.log("CHECKED!");
+            return INPUT.getAttribute("value");
+        }
+    }
+    /*
+        for (var RADIO_GROUP in RADIO_GROUPS)
+        {
+            const INPUT = RADIO_GROUP.querySelector("input");
+            if (INPUT.checked)
+                return INPUT.getAttribute("value");
+        }
+        */
 }
