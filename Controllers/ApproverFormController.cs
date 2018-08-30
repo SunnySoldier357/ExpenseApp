f9b8ac7e653a23e74d63cfb84397b0b3433f435c
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2018-08-29 19:00:16 -0700

    Add ApproverController with working List View

---
+++ Controllers/ApproverFormController.cs
@@ -0,0 +1,49 @@
+﻿using ExpenseApp.Models.DB;
+using ExpenseApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpenseApp.Controllers
+{
+    [Route("approver")]
+    public class ApproverFormController : Controller
+    {
+        public Employee SignedInApprover;
+
+        private readonly ExpenseDBDataContext _db;
+
+        public ApproverFormController(ExpenseDBDataContext db)
+        {
+            _db = db;
+
+            SignedInApprover = _db.Employees
+                .Include(e => e.Location)
+                .FirstOrDefault(e => e.Id == new Guid("9adada3b-2032-4b9d-b23d-afa8e07666da"));
+        }
+
+        [Route("")]
+        public IActionResult List()
+        {
+            var forms = _db.ExpenseForms
+                .Include(ef => ef.Employee)
+                    .ThenInclude(ef => ef.Location)
+                .Where(ef => ef.Employee.Location == SignedInApprover.Location && 
+                    ef.Status != Status.Saved && ef.Status != Status.Rejected)
+                .OrderByDescending(ef => ef.StatementNumber.Substring(2, 2))
+                .ThenByDescending(ef => ef.StatementNumber.Substring(0, 2))
+                .ThenBy(ef => ef.StatementNumber.Substring(5, ef.StatementNumber.Length - 8))
+                .ThenByDescending(ef => ef.StatementNumber.Substring(ef.StatementNumber.Length - 2))
+                .Select(ef => new { ef.StatementNumber, ef.Title, ef.Status, EmployeeName = ef.Employee.FullName });
+
+            var listings = new List<ExpenseListViewModel>();
+
+            foreach (var form in forms)
+                listings.Add(new ExpenseListViewModel(form, true));
+
+            return View(listings);
+        }
+    }
+}
--- ViewModels/ExpenseListViewModel.cs
+++ ViewModels/ExpenseListViewModel.cs
@@ -7,13 +7,24 @@ namespace ExpenseApp.ViewModels
         public string StatementNumber { get; set; }
         public string Title { get; set; }
         public Status Status { get; set; }
-        public string FirstName { get; set; }
+        public string EmployeeName { get; set; }
 
-        public ExpenseListViewModel(dynamic form)
+        public ExpenseListViewModel(dynamic form, bool approverController = false)
         {
             StatementNumber = form.StatementNumber;
             Title = form.Title;
-            Status = form.Status;
+            if (!approverController)
+            {
+                if (form.Status == Status.Paid)
+                    Status = Status.Approved;
+                else
+                    Status = form.Status;
+            }
+            else
+                Status = form.Status;
+
+            if (approverController)
+                EmployeeName = form.EmployeeName;
         }
     }
 }
